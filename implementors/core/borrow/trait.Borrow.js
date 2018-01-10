4a1f7ab7f1a1c4383f1d15a854744f5abf7e67a3
Travis CI User <travis@example.org>
2018-01-10 21:07:11 +0000

    Documentation for moore (f9b9c5f)

---
+++ implementors/core/borrow/trait.Borrow.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["ascii"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;<a class=\"struct\" href=\"ascii/struct.AsciiStr.html\" title=\"struct ascii::AsciiStr\">AsciiStr</a>&gt; for <a class=\"struct\" href=\"ascii/struct.AsciiString.html\" title=\"struct ascii::AsciiString\">AsciiString</a>",];
+implementors["moore_common"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/borrow/trait.Borrow.html\" title=\"trait core::borrow::Borrow\">Borrow</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.str.html\">str</a>&gt; for <a class=\"struct\" href=\"moore_common/name/struct.RcStr.html\" title=\"struct moore_common::name::RcStr\">RcStr</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
